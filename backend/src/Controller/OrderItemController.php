d76a5eda4a56c42d40ae114a74552ff37d4f25db
Jakub <kubajez02@gmail.com>
2025-04-21 19:53:48 +0200

    Background logic

--- backend/src/Controller/OrderItemController.php
+++ backend/src/Controller/OrderItemController.php
@@ -10,50 +10,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class OrderItemController extends AbstractController
 {
     #[Route('api/orderItem', name: 'api_orderItem_index', methods: ['GET'])]
+    #[IsGranted('ROLE_ADMIN')]
     public function index(OrderItemRepository $repository): JsonResponse
     {
         return $this->json($repository->findAll(), JsonResponse::HTTP_OK);
-    }
-    
-    #[Route('api/orderItem/add', name: 'orderItem_add', methods: ['POST'])]
-    public function add(
-        Request $request, 
-        EntityManagerInterface $manager,
-        OrderItemRepository $repository,
-        ProductRepository $productRepository
-    ): JsonResponse {
-        $data = json_decode($request->getContent(), true);
-
-        $order = $repository->find($data['order_id']);
-        $product = $productRepository->find($data['product_id']);
-
-        if (!$order) {
-            return $this->json(
-                ['error' => 'Zamówienie o podanym ID nie zostało znalezione.'],
-                JsonResponse::HTTP_BAD_REQUEST
-            );
-        }
-
-        if (!$product) {
-            return $this->json(
-                ['error' => 'Produkt o podanym ID nie został znaleziony.'],
-                JsonResponse::HTTP_BAD_REQUEST
-            );
-        }
-
-        $orderItem = new OrderItem();
-        $orderItem->setOrder($order)
-                  ->setProduct($product)
-                  ->setQuantity((int) $data['quantity'])
-                  ->setPrice((string) $data['price']);
-
-        $manager->persist($orderItem);
-        $manager->flush();
-
-        return $this->json($orderItem, JsonResponse::HTTP_CREATED);
     }
 }
