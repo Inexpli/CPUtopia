7750c9d47356840f93d1f83a6db3997dd538d015
Sp3EdQQ <bartekjanik45@gmail.com>
2025-04-29 19:35:05 +0200

    feat: docker for frontend

--- frontend/src/components/MainLogo.tsx
+++ frontend/src/components/MainLogo.tsx
@@ -1,10 +1,9 @@
 export const MainLogo = () => {
     return <a href="/" className="flex items-center space-x-3">
         <img
-            src="https://dcassetcdn.com/design_img/3694363/790572/21993133/f99c8f70yc0wgssmzhtk56h4dr_image.png"
-            className="max-h-8"
+            src="public/image.png"
+            className="h-12"
             alt="Logo"
         />
-        <span className="text-2xl font-semibold dark:text-white">CPUtopia</span>
     </a>
 }
