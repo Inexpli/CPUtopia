3cb30cde970451e1aaff0e8d15b0b05e8c5b2965
Jakub <kubajez02@gmail.com>
2025-06-07 16:49:25 +0200

    07.06.2025 Wojna z axiosem koloryzowane

--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -11,6 +11,7 @@ export const useLogin = () => {
                 headers: {
                     'Content-Type': 'application/json',
                 },
+                credentials: 'include',
                 body: JSON.stringify(data),
             });
 
--- frontend/src/hooks/useUserProfile.ts
+++ frontend/src/hooks/useUserProfile.ts
@@ -1,13 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+
 const API_URL = 'http://localhost:8080';
 
 export const useUserProfile = () => {
     return useQuery({
         queryKey: ["userProfile"],
         queryFn: async () => {
-            const response = await axios.get(`${API_URL}/api/user/me`);
+            const response = await axios.get(`${API_URL}/api/user/me`, {
+                withCredentials: true, 
+            });
             return response.data;
         },
     });
-}; 
+};
